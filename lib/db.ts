e03e61021b8332576f3c0526f8271f8aae38112c
meetVaidya <vaidyameet2004@gmail.com>
2024-11-27 19:50:28 +0530

    Refactor database connection to use environment variables for configuration

--- lib/db.ts
+++ lib/db.ts
@@ -1,10 +1,10 @@
 import { Pool } from "pg";
 
-const pool = new Pool({
-  user: "admin", // Username from the image
-  host: "timescaledb", // Host URL from the image
-  database: "financial_transactions", // Database name from the image
-  password: "password", // Replace this with the actual password
+export const pool = new Pool({
+  user: process.env.DB_USER, // Username from the image
+  host: process.env.DB_HOST, // Host URL from the image
+  database: process.env.DB_NAME, // Database name from the image
+  password: process.env.DB_PASSWORD, // Replace this with the actual password
   port: 5432, // Port from the image
 });
 
